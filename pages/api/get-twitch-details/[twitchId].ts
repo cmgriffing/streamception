e05d1ef3d96b2a644fa32a710e2f9de53dab0a8d
Chris Griffing <cmgriffing@gmail.com>
2021-05-07 12:21:12 -0700

    fix: add twitchId to query params for twitch details lookup

--- pages/api/get-twitch-details/[twitchId].ts
+++ pages/api/get-twitch-details/[twitchId].ts
@@ -5,7 +5,7 @@ export default withUser(async (req: NextApiRequestWithUser, res) => {
   const { twitchId } = req.query;
 
   const userResponse: any = await axios
-    .get(`https://api.twitch.tv/helix/users`, {
+    .get(`https://api.twitch.tv/helix/users?id=${twitchId}`, {
       headers: {
         Authorization: `Bearer ${req.token}`,
         "Client-Id": process.env.TWITCH_CLIENT_ID,
